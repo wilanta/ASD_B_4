9e500a179da0534861c248752d18225f73a826d9
Tehfa <fateehganteng@gmail.com>
2026-02-24 16:31:04 +0700

    add[order.py]: orderKursi function, returns jumlah tiket and selected seats

--- CRUD/operations/order.py
+++ CRUD/operations/order.py
@@ -1,7 +1,8 @@
 """
 ==========================================================
 Manajemen Pemesanan Kursi & Manajemen Pemesanan Tiket
-Untuk memilih kursi yang kosong untuk customer secara bebas dan menyimpan pemesanan ke log_antrean.txt.
+Untuk memilih kursi yang kosong untuk customer secara bebas
+dan menyimpan pemesanan ke log_antrean.txt.
 
 Kontributor : Fateeh Falah Hendarto
 Fungsi/fitur:
@@ -14,33 +15,54 @@ Fungsi/fitur:
 # Dependencies
 from rich import print
 
+# Utils
+from CRUD.utils.dataOps import getAllData
+
 # ------------------------------
 # Nama fungsi: orderKursi
 # Penjelasan fungsi : Untuk memilihkan kursi kosong untuk customer secara bebas.
 # ------------------------------
-def orderKursi():
-    with open('../data/data_film.txt', 'r', encoding='utf-8') as file:
-        # Loop setiap baris di file
-        for row in file:
-            row = row.strip()
-            _id_film, _nama_film, kuota_film = row.split(',')
-    print(kuota_film)
+
+
+def orderKursi(kursi_id='ABC123'):  # kursi_id PLACEHOLDER
+    "Order Kursi"
+    data = getAllData('data_film')
     max_kursi_per_cust = 4
     while True:
         try:
             user_ticket = int(input("Masukkan jumlah tiket yang dipesan: "))
             if user_ticket > max_kursi_per_cust or user_ticket < 0:
                 print("Hanya bisa 1-4 tiket!")
-            elif user_ticket > kuota_film:
-                print(f"Hanya tersisa {kuota_film} kuota!")
+            elif user_ticket > int(data[kursi_id]['kuota_penonton']):
+                print(
+                    f"Hanya tersisa {data[kursi_id]['kuota_penonton']} kuota!")
         except ValueError:
             print("Masukkan bilangan yang valid!")
-    # print("=== Pilih Kursi ===")
+        break
+    print("=== Kursi Tersedia ===")
+    available_seats = [
+        "K" + str(i) for i in range(1, int(data[kursi_id]['kuota_penonton']))]
+    print(available_seats)
+    for _ in range(user_ticket):
+        select = input("Pilih Kursi: ").upper().strip()
+        selected_seats = []
+        if select in available_seats:
+            available_seats.remove(select)
+            selected_seats.append(select)
+        else:
+            print("Kursi tidak valid / sudah terpakai.")
+    return user_ticket, selected_seats
+
+
 orderKursi()
+
 # ------------------------------
 # Nama fungsi: storeOrder
-# Penjelasan fungsi : Untuk menyimpan pemesanan customer yang telah dilayani dan memilih kursi ke log_pemesanan.txt dengan format log_id,nama,jumlah_ticket,urutan_antrean,judul.
+# Penjelasan fungsi : Untuk menyimpan pemesanan customer yang telah dilayani dan memilih kursi
+# ke log_pemesanan.txt dengan format log_id,nama,jumlah_ticket,urutan_antrean,judul.
 # ------------------------------
+
+
 def storeOrder():
     pass
 
@@ -48,5 +70,7 @@ def storeOrder():
 # Nama fungsi: resetOrder
 # Penjelasan fungsi : Untuk mereset sistem antrean menjadi 0 antrean kembali.
 # ------------------------------
+
+
 def resetOrder():
     pass
