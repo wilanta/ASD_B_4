03ba5e575511812d1c9fd40d14f32c3e661cab39
Wilantara <dzikriwildhan@gmail.com>
2026-03-07 12:03:20 +0700

    Fix[CRUD/operations/linkedList]: refactor all features

--- CRUD/operations/linkedList.py
+++ CRUD/operations/linkedList.py
@@ -12,37 +12,39 @@ Fungsi/fitur:
 ==========================================================
 """
 
-# Inisialisasi variabel untuk linked list
-class Node:
-    "Node untuk Linked List"
-    def __init__(self, data):
-        self.data = data
-        self.next = None
 
-# ------------------------------
-# Nama fungsi: addTicket
-# Penjelasan fungsi : Untuk menambahkan ticket ke linked list.
-# ------------------------------
-def addTicket(nama, jumlah, kursi_list):
-    pass
+# ````````````````````````````````````````````
+# Nama kelas: LinkedList
+# Penjelasan kelas : Untuk membuat linked list yang akan digunakan untuk menyimpan data dan log pemesanan tiket.
+# ````````````````````````````````````````````
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-# ------------------------------
-# Nama fungsi: showTickets
-# Penjelasan fungsi : Untuk menampilkan semua ticket yang berada dalam linked list.
-# ------------------------------
-def showTickets():
-    pass
+    # ------------------------------
+    # Nama fungsi: addTicket
+    # Penjelasan fungsi : Untuk menambahkan ticket ke linked list.
+    # ------------------------------
+    def addTicket(self, nama, jumlah_tiket, nomor_kursi, urutan_antrean, judul_film):
+        pass
 
-# ------------------------------
-# Nama fungsi: deleteTicket
-# Penjelasan fungsi : Untuk menghapus satu ticket dalam linked list.
-# ------------------------------
-def deleteTicket(nama):
-    pass
+    # ------------------------------
+    # Nama fungsi: showTickets
+    # Penjelasan fungsi : Untuk menampilkan semua ticket yang berada dalam linked list.
+    # ------------------------------
+    def showTickets(self):
+        pass
 
-# ------------------------------
-# Nama fungsi: searchTicket
-# Penjelasan fungsi : Untuk satu mencari ticket dalam linked list.
-# ------------------------------
-def searchTicket(nama):
-    pass
+    # ------------------------------
+    # Nama fungsi: deleteTicket
+    # Penjelasan fungsi : Untuk menghapus satu ticket dalam linked list.
+    # ------------------------------
+    def deleteTicket(self, nama):
+        pass
+
+    # ------------------------------
+    # Nama fungsi: searchTicket
+    # Penjelasan fungsi : Untuk satu mencari ticket dalam linked list.
+    # ------------------------------
+    def searchTicket(self, nama):
+        pass
