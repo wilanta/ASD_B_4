08b0869a3056baec80810629c7efbe6fdc8609f4
Wilantara <dzikriwildhan@gmail.com>
2026-03-07 18:05:02 +0700

    Fix[CRUD/operations/order]: refactor orderKursi code -> remove jumlah_tiket input & add new pilih kursi validator

--- CRUD/operations/order.py
+++ CRUD/operations/order.py
@@ -18,39 +18,26 @@ Fungsi/fitur:
 # ------------------------------
 
 
-def orderKursi(available_seats_list: list):  # kursi_id PLACEHOLDER
+def orderKursi(user_ticket: int, available_seats_list: list):
     "Order Kursi"
-    max_kursi_per_cust = 4  # Constant
-
-    # Loop user input
-    while True:
-        # Input validation (must be int)
-        try:
-            user_ticket = int(input("Masukkan jumlah tiket yang dipesan: "))
-            # If not between 1-4
-            if user_ticket > max_kursi_per_cust or user_ticket < 0:
-                print("Hanya bisa 1-4 tiket!")
-            # Else if over the quota
-            elif user_ticket > len(available_seats_list):
-                print(f"Hanya tersisa {len(available_seats_list)} kuota!")
-        except ValueError:
-            print("Masukkan bilangan yang valid!")
-        break  # exit loop
 
     # Menu
     print("=== Kursi Tersedia ===")
     print(available_seats_list)
+
     # User select seats
+    selected_seats = []  # List to hold user's choice(s)
     for i in range(user_ticket):
-        select = input(f"Pilih Kursi Tiket ke-{i + 1}: ").upper().strip()
-        selected_seats = []  # List to hold user's choice(s)
-        # Transfer seat from available seat list if true
-        # Print error if false
-        if select in available_seats_list:
-            available_seats_list.remove(select)
-            selected_seats.append(select)
-        else:
-            print("Kursi tidak valid / sudah terpakai.")
+        while True:  # Loop until a valid seat is selected
+            select = input(f"\nPilih Kursi Tiket ke-{i + 1}: ").upper().strip()
+            # Transfer seat from available seat list if true
+            if select in available_seats_list:
+                available_seats_list.remove(select)
+                selected_seats.append(select)
+                break
+            # Print error if false
+            else:
+                print("Kursi tidak valid / sudah terpakai.")
     return user_ticket, selected_seats  # Return ticket amount and selected seats
 
 
